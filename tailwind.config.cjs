73e8c44d86f1562d04b3293c90383ea9ec6d0a3b
TadeoMedina <86540309+TadeoMedina@users.noreply.github.com>
2026-02-20 15:22:25 -0300

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
